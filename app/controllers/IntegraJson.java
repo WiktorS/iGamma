1ab81dbb6f1893b5b5d0d4985109ca688f4569e0
Wiktor Stasiak <wstasiak@systherm-info.pl>
2012-08-07 14:05:40 +0200

    Added new mock json data provider

--- app/controllers/IntegraJson.java
+++ app/controllers/IntegraJson.java
@@ -1,17 +1,28 @@
 package controllers;
 
 import integra.Integra;
-import integra.IntegraServer;
 import integra.models.RegisterUnit;
+import play.Play;
+import play.libs.F;
 import play.mvc.Controller;
 
 import java.util.List;
 
 public class IntegraJson extends Controller {
+    private static Integra integra = new Integra(Play.configuration.getProperty("integra.url"));
 
-    public static void getRegisterUnits() {
-        Integra integra = IntegraServer.createServer();
+    public static void getRegisterUnits() throws InterruptedException {
         List<RegisterUnit> result = integra.getRegisterUnits();
+        renderJSON(result);
+    }
+
+    public static void getRegisterUnitsByJrg(String jrg) {
+        List<RegisterUnit> result = integra.getRegisterUnitsByJrg(jrg);
+        renderJSON(result);
+    }
+
+    public static void getRegisterUnitsByJrb(String jrb) {
+        List<RegisterUnit> result = integra.getRegisterUnitsByJrg(jrb);
         renderJSON(result);
     }
 }
