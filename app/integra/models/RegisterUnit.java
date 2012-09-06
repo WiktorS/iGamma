180a0d00fbc2195b5f9acc3337de734ce5beaa3c
WiktorS <wstasiak@systherm-info.pl>
2012-09-06 13:13:57 +0200

    Removed toJson and fromJson methods from IntegraModal interface, since this can be easily done by e.g. Gson class

--- app/integra/models/RegisterUnit.java
+++ app/integra/models/RegisterUnit.java
@@ -1,6 +1,5 @@
 package integra.models;
 
-import com.google.gson.JsonObject;
 
 public class RegisterUnit implements IntegraModel {
     public String marker;
@@ -13,31 +12,9 @@ public class RegisterUnit implements IntegraModel {
     public String creationDate;
     public String regon;
 
-    @Override
-    public void fromJson(JsonObject jsonObject) {
-        marker = jsonObject.get("marker").getAsString();
-        jrgNumber = jsonObject.get("jrgNumber").getAsString();
-        jrbNumber = jsonObject.get("jrbNumber").getAsString();
-        jrlNumber = jsonObject.get("jrlNumber").getAsString();
-        precinct = jsonObject.get("precinct").getAsString();
-        cadastralUnit = jsonObject.get("cadastralUnit").getAsString();
-        creationProof = jsonObject.get("creationProof").getAsString();
-        creationDate = jsonObject.get("creationDate").getAsString();
-        regon = jsonObject.get("regon").getAsString();
-    }
-
-    @Override
-    public JsonObject toJson() {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("marker", marker);
-        jsonObject.addProperty("jrgNumber", jrgNumber);
-        jsonObject.addProperty("jrbNumber", jrbNumber);
-        jsonObject.addProperty("jrlNumber", jrlNumber);
-        jsonObject.addProperty("precinct", precinct);
-        jsonObject.addProperty("cadastralUnit", cadastralUnit);
-        jsonObject.addProperty("creationProof", creationProof);
-        jsonObject.addProperty("creationDate", creationDate);
-        jsonObject.addProperty("regon", regon);
-        return jsonObject;
-    }
+    public String area;
+    public String kw;
+    public String householdType;
+    public String remarks;
+    public String notes;
 }
