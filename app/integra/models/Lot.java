31b5c394c92c28432a9bd742893f333b181ca92e
pawelch <pawel.chmielarz74@gmail.com>
2012-09-19 16:04:32 +0200

    Merge remote-tracking branch 'wiktorGamma/master'
    Conflicts:
    	public/javascripts/app/controllers/EgbilSearchLotsController.coffee

---
+++ app/integra/models/Lot.java
@@ -0,0 +1,20 @@
+package integra.models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pchmielarz
+ * Date: 17.09.12
+ * Time: 01:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class Lot implements IntegraModel {
+    public String marker;
+    public String lotNumber;
+    public String jrgNumber;
+    public String precinct;
+    public String cadastralSheet;
+    public String cadastralUnit;
+    public String cadastralArea;
+    public String address;
+    public String document;
+}
