8859c8f13b62d07db14594076a95d4201ec4d23e
Wiktor Stasiak <wstasiak@systherm-info.pl>
2012-10-24 14:50:10 +0200

    660271-37332489 Pasek funkcyjny obiektu Custom report modal - configuration data model

---
+++ app/integra/models/CustomReportConfiguration.java
@@ -0,0 +1,8 @@
+package integra.models;
+
+import java.util.List;
+
+public class CustomReportConfiguration {
+    public String name;
+    public List<CustomReportAttribute> selectedAttributeList;
+}
---
+++ app/integra/models/CustomReportData.java
@@ -0,0 +1,9 @@
+package integra.models;
+
+import java.util.List;
+
+public class CustomReportData {
+    public List<CustomReportAttribute> attributeList;
+    public List<String> unitList;
+    public List<CustomReportConfiguration> configurationList;
+}
