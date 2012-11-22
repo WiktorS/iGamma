b334094dc217b2942d38031a57eb7cddb90c4c52
Wiktor Stasiak <wstasiak@systherm-info.pl>
2012-11-22 15:12:51 +0100

    Removed unused code - mock with data from local database

--- app/PlayBootstrapJob.java
+++ app/PlayBootstrapJob.java
@@ -1,17 +1,11 @@
-import models.RegisterUnitMock;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
-import play.test.Fixtures;
 
 @OnApplicationStart
 public class PlayBootstrapJob extends Job {
 
     @Override
     public void doJob() {
-        if (RegisterUnitMock.count()==0) {
-            Fixtures.deleteAllModels();
-            Fixtures.loadModels("initial-data.yml");
-        }
     }
 
 }
