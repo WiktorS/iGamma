3e252520a1cfb4a4d7d587a521a324ff545f85fb
Wiktor Stasiak <wstasiak@systherm-info.pl>
2013-02-06 10:20:16 +0100

    [5231] Missing ID member in Building model

--- app/integra/models/Building.java
+++ app/integra/models/Building.java
@@ -1,13 +1,7 @@
 package integra.models;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pchmielarz
- * Date: 21.09.12
- * Time: 02:07
- * To change this template use File | Settings | File Templates.
- */
 public class Building implements IntegraModel {
+    public long id;
     public String marker;
     public String buildingNumber;
     public String lotNumber;
