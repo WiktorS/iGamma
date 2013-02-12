2fa9f7bb963c3174b099daebc0f1ef9eb51921f4
Wiktor Stasiak <wstasiak@systherm-info.pl>
2013-02-12 14:38:41 +0100

    IntegraMock checks data integrity between java classes and data in Json mock files

--- app/integra/IntegraMock.java
+++ app/integra/IntegraMock.java
@@ -1,7 +1,6 @@
 package integra;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import integra.models.*;
 import play.Logger;
@@ -10,23 +9,73 @@ import play.Play;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.Reader;
+import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class IntegraMock implements Integra {
 
     private static final Gson gson = new GsonBuilder().excludeFieldsWithModifiers(Modifier.PRIVATE).create();
 
-    private <T> T getMockJsonData(String methodName, TypeToken<T> typeToken, String... params) {
+    private void mockIntegrityChecker(Type type, Reader json) throws Exception{
+        checkElement(new JsonParser().parse(json), type);
+    }
+
+    private void checkElement(JsonElement jsonElement, Type type) throws Exception {
+        if (jsonElement.isJsonArray()) {
+            checkArray(jsonElement.getAsJsonArray(), type);
+        }
+        else if (jsonElement.isJsonObject()) {
+            checkObject(jsonElement.getAsJsonObject(), type);
+        }
+        else if  (jsonElement.isJsonPrimitive()) {
+            checkPrimitive(jsonElement.getAsJsonPrimitive(), type);
+        }
+    }
+
+    private void checkObject(JsonObject jsonObject, Type type) throws Exception {
+        for (Map.Entry<String, JsonElement> stringJsonElementEntry : jsonObject.entrySet()) {
+            String fieldName = stringJsonElementEntry.getKey();
+            Field field;
+            try{
+                field =((Class)type).getField(fieldName);
+            }
+            catch (NoSuchFieldException e) {
+                throw new Exception("Java class '"+((Class) type).getName()+"' is missing field: '"+fieldName+"'");
+            }
+            checkElement(stringJsonElementEntry.getValue(), field.getGenericType());
+        }
+        for (Field field : ((Class) type).getFields()) {
+            String fieldName = field.getName();
+            if (jsonObject.get(fieldName) == null)
+                throw new Exception("JSON object has field '"+fieldName+"' which is not defined in class '"+((Class) type).getName()+"'");
+        }
+
+    }
+
+    private void checkArray(JsonArray jsonArray, Type type) throws Exception {
+        ParameterizedType parametrisedType = (ParameterizedType)type;
+        for (JsonElement arrayElement : jsonArray) {
+            Type[] actualTypeArguments = parametrisedType.getActualTypeArguments();
+            checkElement(arrayElement, actualTypeArguments[0]);
+        }
+    }
+
+    private void checkPrimitive(JsonPrimitive jsonPrimitive, Type type) throws Exception {
+        //TODO: Check primitive types?
+    }
+
+    private <T> T getMockJsonData(String methodName, TypeToken<T> typeToken, String... params) throws Exception{
         BufferedReader bufferedReader;
         T result = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(getMockFile(methodName, params)));
-            result = gson.fromJson(bufferedReader, typeToken.getType());
-            bufferedReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        bufferedReader = new BufferedReader(new FileReader(getMockFile(methodName, params)));
+        //Test if JSON and Type fields are matching
+        mockIntegrityChecker(typeToken.getType(), bufferedReader);
+        result = gson.fromJson(bufferedReader, typeToken.getType());
+        bufferedReader.close();
         return result;
     }
 
@@ -91,13 +140,13 @@ public class IntegraMock implements Integra {
 
 
     @Override
-    public List<RegisterUnit> getRegisterUnits(String type) {
+    public List<RegisterUnit> getRegisterUnits(String type) throws Exception {
         TypeToken<List<RegisterUnit>> typeToken = new TypeToken<List<RegisterUnit>>() {};
         return getMockJsonData("getRegisterUnits", typeToken, type);
     }
 
     @Override
-    public List<Lot> getLotsByNumber(String number) {
+    public List<Lot> getLotsByNumber(String number) throws Exception {
         TypeToken<List<Lot>> typeToken = new TypeToken<List<Lot>>() {};
         return getMockJsonData("getLotsByNumber", typeToken, number);
     }
@@ -105,72 +154,82 @@ public class IntegraMock implements Integra {
     @Override
     public List<Building> getBuildings(long idList[]) throws Exception {
         TypeToken<List<Building>> typeToken = new TypeToken<List<Building>>() {};
+        if (idList == null)
+            throw new Exception("List is null");
+        if (idList.length == 0)
+            throw new Exception("List is empty");
+        if (idList != null) {
+            for (long item: idList) {
+                if (item < 0)
+                    throw new Exception("Wrong identifier");
+            }
+        }
         Arrays.sort(idList);
         return getMockJsonData("getBuildings", typeToken, Arrays.toString(idList));
     }
 
     @Override
-    public List<Local> getLocalByNumber(String numberL) {
+    public List<Local> getLocalByNumber(String numberL) throws Exception {
         TypeToken<List<Local>> typeToken = new TypeToken<List<Local>>() {};
         return getMockJsonData("getLocalByNumber", typeToken, numberL);
     }
 
     @Override
-    public List<Person> getPersonByPesel(String pesel) {
+    public List<Person> getPersonByPesel(String pesel) throws Exception {
         TypeToken<List<Person>> typeToken = new TypeToken<List<Person>>() {};
         return getMockJsonData("getPersonByPesel", typeToken, pesel);
     }
 
     @Override
-    public List<Institution> getInstitutionByNip(String nip) {
+    public List<Institution> getInstitutionByNip(String nip) throws Exception {
         TypeToken<List<Institution>> typeToken = new TypeToken<List<Institution>>() {};
         return getMockJsonData("getInstitutionByNip", typeToken, nip);
     }
 
     @Override
-    public List<Group> getGroupByNip(String nip) {
+    public List<Group> getGroupByNip(String nip) throws Exception {
         TypeToken<List<Group>> typeToken = new TypeToken<List<Group>>() {};
         return getMockJsonData("getGroupByNip", typeToken, nip);
     }
 
     @Override
-    public List<LandCommunity> getLandCommunities(String name) {
+    public List<LandCommunity> getLandCommunities(String name) throws Exception {
         TypeToken<List<LandCommunity>> typeToken = new TypeToken<List<LandCommunity>>() {};
         return getMockJsonData("getLandCommunities", typeToken, name);
     }
 
     @Override
-    public EgbilObject getEgbilObject(String type, String name) {
+    public EgbilObject getEgbilObject(String type, String name) throws Exception {
         TypeToken<EgbilObject> typeToken = new TypeToken<EgbilObject>() {};
         return getMockJsonData("getEgbilObject", typeToken, type, name);
     }
 
     @Override
-    public List<EgbilDocument> getDocuments() {
+    public List<EgbilDocument> getDocuments() throws Exception {
         TypeToken<List<EgbilDocument>> typeToken = new TypeToken<List<EgbilDocument>>() {};
         return getMockJsonData("getDocuments", typeToken);
     }
 
     @Override
-    public List<Systematic> getSystematics(long[] parentIdList) {
+    public List<Systematic> getSystematics(long[] parentIdList) throws Exception {
         TypeToken<List<Systematic>> typeToken = new TypeToken<List<Systematic>>() {};
         return getMockJsonData("getSystematics", typeToken, Arrays.toString(parentIdList));
     }
 
     @Override
-    public List<ShareSummary> getShareSummary() {
+    public List<ShareSummary> getShareSummary() throws Exception {
         TypeToken<List<ShareSummary>> typeToken = new TypeToken<List<ShareSummary>>() {};
         return getMockJsonData("getShareSummary", typeToken);
     }
 
     @Override
-    public CustomReportData getCustomReportData(String type, String name) {
+    public CustomReportData getCustomReportData(String type, String name) throws Exception {
         TypeToken<CustomReportData> typeToken = new TypeToken<CustomReportData>() {};
         return getMockJsonData("getCustomReportData", typeToken, type, name);
     }
 
     @Override
-    public List<EgbilObjectData> getRelatedObjects(String relation, EgbilObjectData[] objectList) {
+    public List<EgbilObjectData> getRelatedObjects(String relation, EgbilObjectData[] objectList) throws Exception {
         TypeToken<List<EgbilObjectData>> typeToken = new TypeToken<List<EgbilObjectData>>() {};
         return getMockJsonData("getRelatedObjects", typeToken, relation, objectList.toString());
     }
@@ -178,6 +237,10 @@ public class IntegraMock implements Integra {
     @Override
     public List<Long> findObjects(String queryName, QueryEntry[] queryEntryList) throws Exception {
         TypeToken<List<Long>> typeToken = new TypeToken<List<Long>>() {};
+        if (queryEntryList == null)
+            throw new Exception("queryEntryList is null");
+        if (queryEntryList.length == 0)
+            throw new Exception("queryEntryList is empty");
         return getMockJsonData("findObjects", typeToken, queryName, queryEntryListToString(queryEntryList));
     }
 }
