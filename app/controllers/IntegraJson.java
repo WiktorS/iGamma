55e24072625929c9d5e56338170b7d7344d553db
Wiktor Stasiak <wstasiak@systherm-info.pl>
2013-03-01 09:36:06 +0100

    [5231] New JSON interface

--- app/controllers/IntegraJson.java
+++ app/controllers/IntegraJson.java
@@ -2,7 +2,7 @@ package controllers;
 
 import integra.Integra;
 import integra.IntegraServer;
-import integra.QueryEntry;
+import integra.models.QueryEntry;
 import integra.models.*;
 import play.mvc.Controller;
 
@@ -32,53 +32,78 @@ public class IntegraJson extends Controller {
         renderJSON(result);
     }
 
-    public static void getRegisterUnits(String type) throws Exception {
-        List<RegisterUnit> result = integra.getRegisterUnits(type);
-        renderJSON(result);
-    }
-
-    public static void getLotsByNumber(String number) throws Exception {
-        List<Lot> result = integra.getLotsByNumber(number);
-        renderJSON(result);
-    }
-
     public static void getBuildings(long idList[]) throws Exception {
         List<Building> result = integra.getBuildings(idList);
         renderJSON(result);
     }
 
-    public static void getLocalByNumber(String numberL) throws Exception {
-        List<Local> result = integra.getLocalByNumber(numberL);
+    public static void getDocuments(long idList[]) throws Exception {
+       List<Document> result = integra.getDocuments(idList);
+       renderJSON(result);
+    }
+
+    public static void getGroups(long idList[]) throws Exception {
+       List<Group> result = integra.getGroups(idList);
+       renderJSON(result);
+    }
+
+    public static void getPersons(long idList[]) throws Exception {
+       List<Person> result = integra.getPersons(idList);
+       renderJSON(result);
+    }
+
+    public static void getInstitutions(long idList[]) throws Exception {
+       List<Institution> result = integra.getInstitutions(idList);
+       renderJSON(result);
+    }
+
+    public static void getJrg(long idList[]) throws Exception {
+       List<RegisterUnit> result = integra.getJrg(idList);
+       renderJSON(result);
+    }
+
+    public static void getJrb(long idList[]) throws Exception {
+       List<RegisterUnit> result = integra.getJrb(idList);
+       renderJSON(result);
+    }
+    
+    public static void getJrl(long idList[]) throws Exception {
+       List<RegisterUnit> result = integra.getJrl(idList);
+       renderJSON(result);
+    }
+
+    public static void getOldJrg(long idList[]) throws Exception {
+       List<RegisterUnit> result = integra.getOldJrg(idList);
+       renderJSON(result);
+    }
+
+    public static void getLandCommunities(long idList[]) throws Exception {
+       List<LandCommunity> result = integra.getLandCommunities(idList);
+       renderJSON(result);
+    }
+
+    public static void getLocals(long idList[]) throws Exception {
+       List<Local> result = integra.getLocals(idList);
+       renderJSON(result);
+    }
+
+    public static void getLots(long idList[]) throws Exception {
+       List<Lot> result = integra.getLots(idList);
+       renderJSON(result);
+    }
+
+    public static void getAddresses(long idList[]) throws Exception {
+       List<Address> result = integra.getAddresses(idList);
+       renderJSON(result);
+    }
+
+    public static void getShares(long idList[]) throws Exception {
+        List<Share> result = integra.getShares(idList);
         renderJSON(result);
     }
 
-    public static void getPersonByPesel(String pesel) throws Exception {
-        List<Person> result = integra.getPersonByPesel(pesel);
-        renderJSON(result);
-    }
-
-    public static void getInstitutionByNip(String nip) throws Exception {
-        List<Institution> result = integra.getInstitutionByNip(nip);
-        renderJSON(result);
-    }
-
-    public static void getGroupByNip(String nip) throws Exception {
-        List<Group> result = integra.getGroupByNip(nip);
-        renderJSON(result);
-    }
-
-    public static void getLandCommunities(String name) throws Exception {
-        List<LandCommunity> result = integra.getLandCommunities(name);
-        renderJSON(result);
-    }
-
-    public static void getEgbilObject(String type, String name) throws Exception {
-        EgbilObject result = integra.getEgbilObject(type, name);
-        renderJSON(result);
-    }
-
-    public static void getDocuments(String unknown) throws Exception {
-        List<EgbilDocument> result = integra.getDocuments();
+    public static void getChanges(long idList[]) throws Exception {
+        List<Change> result = integra.getChanges(idList);
         renderJSON(result);
     }
 
@@ -87,20 +112,20 @@ public class IntegraJson extends Controller {
         renderJSON(result);
     }
 
-    public static void getShareSummary() throws Exception {
-        List<ShareSummary> result = integra.getShareSummary();
-        renderJSON(result);
-    }
+//    public static void getShareSummary() throws Exception {
+//       List<ShareSummary> result = integra.getShareSummary();
+//       renderJSON(result);
+//    }
+//
+//    public static void getCustomReportData(String type, String name) throws Exception {
+//       CustomReportData result = integra.getCustomReportData(type, name);
+//       renderJSON(result);
+//    }
 
-    public static void getCustomReportData(String type, String name) throws Exception {
-        CustomReportData result = integra.getCustomReportData(type, name);
-        renderJSON(result);
-    }
-
-    public static void getRelatedObjects(String relation, EgbilObjectData[] objectList) throws Exception {
-        List<EgbilObjectData> result = integra.getRelatedObjects(relation, objectList);
-        renderJSON(result);
-    }
+    // public static void getRelatedObjects(String relation, EgbilObjectData[] objectList) throws Exception {
+    //    List<EgbilObjectData> result = integra.getRelatedObjects(relation, objectList);
+    //    renderJSON(result);
+    // }
 
     public static void getTerrainCategorySummary(String objectType, String objectName) throws Exception {
         renderText("ZESTAWIENIE KLASOUŻYTKÓW");
@@ -110,9 +135,9 @@ public class IntegraJson extends Controller {
         renderText("REZERWACJA NUMERÓW");
     }
 
-    public static void getCustomReport(Boolean attributeLegend, String lineBreak, String fileFormat, CustomReportAttribute[] attributeList) throws Exception {
-        renderText("RAPORT DOWOLNY");
-    }
+    // public static void getCustomReport(Boolean attributeLegend, String lineBreak, String fileFormat, CustomReportAttribute[] attributeList) throws Exception {
+    //    renderText("RAPORT DOWOLNY");
+    // }
 
     public static void getDifferenceReport(String reportType, Boolean attribute, Boolean registerUnit, Boolean share, Boolean lot, Boolean building, Boolean local) throws Exception {
         renderText("RAPORT RÓŻNIC");
